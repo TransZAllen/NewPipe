1fe8779f296458b91adab011a75c54c8585df9e9
TransZAllen <tree.story@outlook.com>
2026-02-05 22:49:03 +0800

    [duplicated subtitle] Update `import` statements after moving from NewPipeExtractor to NewPipe main repository.

--- app/src/main/java/us/shandian/giga/get/DownloadInitializer.java
+++ app/src/main/java/us/shandian/giga/get/DownloadInitializer.java
@@ -19,7 +19,7 @@ import us.shandian.giga.util.Utility;
 
 import static org.schabi.newpipe.BuildConfig.DEBUG;
 import static us.shandian.giga.get.DownloadMission.ERROR_HTTP_FORBIDDEN;
-import org.schabi.newpipe.extractor.utils.SubtitleDeduplicator;
+import org.schabi.newpipe.player.subtitles.SubtitleDeduplicator;
 
 public class DownloadInitializer extends Thread {
     private static final String TAG = "DownloadInitializer";
