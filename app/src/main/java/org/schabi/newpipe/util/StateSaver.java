b9913603529c4b6c7429ead988cfc50a7499fd4c
TransZAllen <tree.story@outlook.com>
2026-02-08 12:28:48 +0800

    Revert "[duplicated subtitle] Initialize subtitle cache directory for `SubtitleDeduplicator`"
    This reverts commit a8738b163e517f4916d848091ae7926f74eff2a4.

--- app/src/main/java/org/schabi/newpipe/util/StateSaver.java
+++ app/src/main/java/org/schabi/newpipe/util/StateSaver.java
@@ -31,7 +31,6 @@ import androidx.core.os.BundleCompat;
 
 import org.schabi.newpipe.BuildConfig;
 import org.schabi.newpipe.MainActivity;
-import org.schabi.newpipe.extractor.utils.SubtitleDeduplicator;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -71,8 +70,6 @@ public final class StateSaver {
         if (TextUtils.isEmpty(cacheDirPath)) {
             cacheDirPath = context.getCacheDir().getAbsolutePath();
         }
-
-        SubtitleDeduplicator.setCacheDirPath(cacheDirPath);
     }
 
     /**
