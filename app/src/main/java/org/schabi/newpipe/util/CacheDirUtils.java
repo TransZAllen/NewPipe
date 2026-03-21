f4edfba53c9d454455afd801afeb77b8ad49c314
TransZAllen <49811617+TransZAllen@users.noreply.github.com>
2026-03-21 16:44:06 +0800

    refactor(CacheDirUtils): Make getInternalAppCacheDirPath() private
    - This method is not intended for external use
    - No functional change

--- app/src/main/java/org/schabi/newpipe/util/CacheDirUtils.java
+++ app/src/main/java/org/schabi/newpipe/util/CacheDirUtils.java
@@ -22,7 +22,7 @@ public final class CacheDirUtils {
         return null;
     }
 
-    public static String getInternalAppCacheDirPath(
+    private static String getInternalAppCacheDirPath(
                                 @NonNull final Context context) {
         // always available, never be 'null'
         // /data/user/0/<package_name>/cache/
