b50036214687350e34f4cf7654338cd506123b23
TransZAllen <49811617+TransZAllen@users.noreply.github.com>
2026-03-21 21:58:09 +0800

    refactor(StateSaver): use CacheDirUtils for "state_cache" cache dir selection
    - replace original cache dir selection in StateSaver.init()
      with CacheDirUtils.getPreferredAppCacheDirPath()
    - No functional changes

--- app/src/main/java/org/schabi/newpipe/util/StateSaver.java
+++ app/src/main/java/org/schabi/newpipe/util/StateSaver.java
@@ -63,13 +63,7 @@ public final class StateSaver {
      * @param context used to get the available cache dir
      */
     public static void init(final Context context) {
-        final File externalCacheDir = context.getExternalCacheDir();
-        if (externalCacheDir != null) {
-            cacheDirPath = externalCacheDir.getAbsolutePath();
-        }
-        if (TextUtils.isEmpty(cacheDirPath)) {
-            cacheDirPath = context.getCacheDir().getAbsolutePath();
-        }
+        cacheDirPath = CacheDirUtils.getPreferredAppCacheDirPath(context);
     }
 
     /**
